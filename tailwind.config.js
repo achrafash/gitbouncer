725a216c5a062b32c978d89cf8c33aa5f4cb1c77
achrafash <aitsidihammou.achraf@gmail.com>
2022-04-02 18:43:34 +0100

    🎨 setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            sans: [
+                "JetBrains Mono",
+                "system-ui",
+                "-apple-system",
+                "Segoe UI",
+                "Roboto",
+            ],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
