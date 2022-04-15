3eabf4d15a662f59ce6b7541e68b08db1a71cc5d
achrafash <aitsidihammou.achraf@gmail.com>
2022-04-15 11:37:10 +0100

    🔇 chore: remove db logs in production

--- utils/db.ts
+++ utils/db.ts
@@ -9,7 +9,10 @@ declare global {
 const prisma =
     global.prisma ||
     new PrismaClient({
-        log: ["error", "info", "query"],
+        log:
+            process.env.NODE_ENV === "production"
+                ? []
+                : ["error", "info", "query"],
     })
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma
